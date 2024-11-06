0e5bf53d4628700665603f517e22790559816885
aloobun <145441993+aloobun@users.noreply.github.com>
2024-11-06 19:03:22 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gadit",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[],
+    description="Basic math library for 2D vector and geometry operations(for Pygame).",
+    author="aloobun",
+    url="https://github.com/aloobun/gadit",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
